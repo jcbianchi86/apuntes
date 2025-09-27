cf69b06d49ead9bdda891e17c80e8ada18cd1054
Juan Carlos <jcbianchi86@gmail.com>
2025-09-26 21:17:26 -0300

    seguimos practicando

---
+++ 03-input.js
@@ -0,0 +1,6 @@
+let bienvenida = "Bienvenidos al Prodrama de Muestra"
+console.log(bienvenida)
+let nombre = "Sofía Malena"
+let edad = 5
+console.log("Gracias " + nombre + ", te damos la bienvenida. Usted tiene " + edad + " años.")
+
--- practica.js
+++ practica.js
@@ -1,10 +1,3 @@
-// Hagan una funcion que reciba nombre, apellido y un callback
-// Dentro la funcion que acaban de crear, definan una variable nombreCompleto combinando el nombre 
-// y el apellido
-// Pasar nombreCompleto como argumento de la funcion callback que recibieron por parametro en el punto 1.
-
-// La funciona callback que pasan a la funcion de orden superior, 
-// debe recibir por parametro un nombreCompleto y retornar "Hola " + nombreCompleto
 
 
 function saludarNombreCompleto(nombre, apellido, callback){
