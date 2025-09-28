14d1e96fb2ced4520d12ddde44240b7d7bfa5a9d
Juan Carlos <jcbianchi86@gmail.com>
2025-09-28 08:47:11 -0300

    más teoría

--- 03-input.js
+++ 03-input.js
@@ -3,4 +3,9 @@ console.log(bienvenida)
 let nombre = "Sofía Malena"
 let edad = 5
 console.log("Gracias " + nombre + ", te damos la bienvenida. Usted tiene " + edad + " años.")
+if(edad < 18){
+    console.log("Usted es menor de edad")
+} else {
+    console.log("Usted es mayor de edad")
+}
 
---
+++ 04-arrays.js
@@ -0,0 +1,26 @@
+let materias = ["Matemática", "Lengua", "Biología"]
+
+//el Array es Ordenado
+
+console.log(materias)
+console.log(materias[1])
+
+//Métodos Comunes
+
+//Push
+
+materias.push("Geografía")
+console.log(materias)
+console.log(materias.length)
+
+//Shift
+
+materias.shift("Geografía")
+console.log(materias)
+console.log(materias.length)
+
+//Slice
+
+
+console.log(materias.slice(1,3))
+console.log(materias.length)
